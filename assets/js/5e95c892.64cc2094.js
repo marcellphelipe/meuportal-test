faf75f5e10572257b403af2dbadaf20e2ed3d3fb
marcell.ribeiro <marcell.ribeiro@equals.com.br>
2024-12-04 16:32:22 -0300

    mais um commit

---
+++ assets/js/5e95c892.64cc2094.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmeu_site=self.webpackChunkmeu_site||[]).push([[9647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>l});r(6540);var u=r(4164),t=r(1213),a=r(7559),c=r(2831),n=r(781),i=r(4848);function l(e){return(0,i.jsx)(t.e3,{className:(0,u.A)(a.G.wrapper.docsPages),children:(0,i.jsx)(n.A,{children:(0,c.v)(e.route.routes)})})}}}]);
---
+++ assets/js/acecf23e.a624f0da.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmeu_site=self.webpackChunkmeu_site||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
